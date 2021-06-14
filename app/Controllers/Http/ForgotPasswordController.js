d8457c3b2b6de8f5da40c080c6b17b59f2d1435c
João Pedro <pedrobarros2010@gmail.com>
2021-06-14 12:04:44 -0300

    feat: add ForgotPassword JSDoc

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,9 +1,20 @@
 'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
 
 class ForgotPasswordController {
+  /**
+   * Creates a new token and sends a email with it
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
   async store ({ request, response }) {
     try {
       const email = request.input('email')
@@ -25,6 +36,42 @@ class ForgotPasswordController {
     } catch (error) {
       console.log(error)
       return response.status(error.status).send({ error: 'no user found with this email' })
+    }
+  }
+
+  /**
+   * Updates the password validating the token in the database
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.only(['token', 'password'])
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenCreatedAtMili = new Date(user.token_created_at).getTime()
+      const currentMili = new Date().getTime()
+
+      // changing the first number can change the duration in days
+      const tokenMiliValidity = 1 * 24 * 60 * 60 * 1000
+      const isTokenValid = currentMili - tokenCreatedAtMili < tokenMiliValidity
+
+      if (!isTokenValid) {
+        return response.status(404).send({ error: 'token expired' })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      user.save()
+
+      return response.status(200)
+    } catch (error) {
+      return response.status(error.status).send({ error: 'an error occurred when reseting the password' })
     }
   }
 }

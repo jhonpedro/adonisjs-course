5eff3b060b7815b619086e489ad629e2264f4e09
João Pedro <pedrobarros2010@gmail.com>
2021-06-14 12:03:53 -0300

    feat: add session JSDoc

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,6 +1,18 @@
 'use strict'
 
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/auth/src/Schemes/Api')} Auth */
+
 class SessionController {
+  /**
+   * Register a new session for the user
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {Auth} ctx.auth
+   */
   async store ({ request, response, auth }) {
     const { email, password } = request.only(['email', 'password'])
 

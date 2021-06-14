a6250696cec7e99695103ea73dc50d975a5a3bf6
João Pedro <pedrobarros2010@gmail.com>
2021-06-14 12:04:28 -0300

    feat: add User JSDoc

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,8 +1,16 @@
 'use strict'
 
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+
 const User = use('App/Models/User')
 
 class UserController {
+  /**
+   * Creates and store a new user
+   *
+   * @param {Object} ctx
+   * @param {Request} ctx.request
+   */
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password'])
 

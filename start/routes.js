b0c2dd1bbad64fe593cf14f57b78b9d7ea29cb75
João Pedro <pedrobarros2010@gmail.com>
2021-06-15 12:01:05 -0300

    feat: applying validators

--- start/routes.js
+++ start/routes.js
@@ -17,16 +17,23 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store').validator('User')
-Route.post('sessions', 'SessionController.store')
+Route.post('sessions', 'SessionController.store').validator('Session')
 
-Route.post('passwords', 'ForgotPasswordController.store')
-Route.put('passwords', 'ForgotPasswordController.update')
+Route.post('passwords', 'ForgotPasswordController.store').validator('ForgotPassword')
+Route.put('passwords', 'ForgotPasswordController.update').validator('ResetPassword')
 
 Route.get('/file/:id', 'FileController.get')
 
-Route.group('auth required', () => {
+Route.group(null, () => {
   Route.post('file', 'FileController.store')
 
-  Route.resource('projects', 'ProjectController').apiOnly()
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([
+      [['projects.store'], ['Project']]
+    ]))
+
+  Route.resource('projects.tasks', 'TaskController')
+    .apiOnly()
+    .validator(new Map([['projects.tasks.store'], ['Task']]))
 }).middleware('auth')

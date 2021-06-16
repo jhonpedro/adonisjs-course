5c21972b6ef16fdc7a302b0176d6e6f0ea60bfa4
João Pedro <pedrobarros2010@gmail.com>
2021-06-16 11:13:45 -0300

    feat: create user addresses on user creation

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,7 +1,10 @@
 'use strict'
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
 
+/** @type {import('@adonisjs/lucid/src/Database/')} */
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
@@ -10,13 +13,23 @@ class UserController {
    *
    * @param {Object} ctx
    * @param {Request} ctx.request
+   * @param {Response} ctx.response
    */
-  async store ({ request }) {
+  async store ({ request, response }) {
     const data = request.only(['username', 'email', 'password'])
+    const adresses = request.input('adresses', [])
 
-    const user = await User.create(data)
+    const trx = await Database.beginTransaction()
+    try {
+      const user = User.create(data, trx)
+      await user.adresses().createMany(adresses, trx)
 
-    return user
+      await trx.commit()
+      return response.send({ ...user })
+    } catch (error) {
+      await trx.rollback()
+      throw error
+    }
   }
 }
 
